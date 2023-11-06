d31fb7d7977989d15763f2134519987ef3dadfc3
MUTInnovationLab <mutinnovationlab@gmail.com>
2023-11-06 11:06:47 +0200

    home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router, private loadingController: LoadingController) {}
+
+  async ngOnInit() {
+    setTimeout(() => {
+      this.router.navigate(['/login']); // Navigate to the login page after a delay
+    }, 3000);
+  }
+  
 
 }
