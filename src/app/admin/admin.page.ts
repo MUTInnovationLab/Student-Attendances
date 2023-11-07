167dbbd24216ad57c5cdbccec38b0bbd4e2fecb1
MUTInnovationLab <mutinnovationlab@gmail.com>
2023-11-07 10:55:04 +0200

    ADDING LECTURER

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { LoadingController } from '@ionic/angular';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.page.html',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPage implements OnInit {
 
-  constructor() { }
+  fullName: string = '';
+  staffNumber: string = '';
+  email: string = '';
+  position: string = '';
+  department: string = '';
+
+
+  constructor(private afs: AngularFirestore,private loadingController: LoadingController) { }
 
   ngOnInit() {
+  }
+
+  async submitForm() {
+    const loader = await this.loadingController.create({
+      message: 'Submitting...',
+      cssClass: 'custom-loader-class',
+    });
+    await loader.present();
+
+    try {
+      const data = {
+        fullName: this.fullName,
+        staffNumber: this.staffNumber,
+        email: this.email,
+        position: this.position,
+        department: this.department,
+      };
+
+      await this.afs.collection('registered staff').add(data);
+
+      // Clear the input fields
+      this.fullName = '';
+      this.staffNumber = '';
+      this.email = '';
+      this.position = '';
+      this.department = '';
+
+      loader.dismiss();
+      alert('Information successfully saved');
+    } catch (error) {
+      loader.dismiss();
+      console.error('Error saving data:', error);
+      alert('An error occurred while saving the information.');
+    }
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -17,7 +18,7 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp(environment.firebaseConfig)
+  imports: [FormsModule,BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp(environment.firebaseConfig)
     ,AngularFirestoreModule,AngularFireAuthModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
