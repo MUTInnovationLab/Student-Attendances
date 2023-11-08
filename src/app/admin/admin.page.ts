41527acd0ee3d3a5e97e951b0a320478f10af4ed
Lindani456 <lindanimlindos345@gmail.com>
2023-11-08 09:52:45 +0200

    login

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { LoadingController } from '@ionic/angular';
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.page.html',
@@ -15,43 +20,106 @@ export class AdminPage implements OnInit {
   department: string = '';
 
 
-  constructor(private afs: AngularFirestore,private loadingController: LoadingController) { }
+  constructor(private alertController: AlertController, private loadingController: LoadingController,
+    private router: Router, private auth: AngularFireAuth, private toastController: ToastController,
+    private navCtrl: NavController, private firestore: AngularFirestore) { }
 
   ngOnInit() {
   }
 
-  async submitForm() {
+  // async submitForm() {
+  //   const loader = await this.loadingController.create({
+  //     message: 'Submitting...',
+  //     cssClass: 'custom-loader-class',
+  //   });
+  //   await loader.present();
+
+  //   try {
+  //     const data = {
+  //       fullName: this.fullName,
+  //       staffNumber: this.staffNumber,
+  //       email: this.email,
+  //       position: this.position,
+  //       department: this.department,
+  //     };
+
+  //     await this.afs.collection('lecturer').add(data);
+
+  //     // Clear the input fields
+  //     this.fullName = '';
+  //     this.staffNumber = '';
+  //     this.email = '';
+  //     this.position = '';
+  //     this.department = '';
+
+  //     loader.dismiss();
+  //     alert('Information successfully saved');
+  //   } catch (error) {
+  //     loader.dismiss();
+  //     console.error('Error saving data:', error);
+  //     alert('An error occurred while saving the information.');
+  //   }
+  // }
+
+  async submitForm(){
+
     const loader = await this.loadingController.create({
-      message: 'Submitting...',
-      cssClass: 'custom-loader-class',
+      message: 'Signing up',
+      cssClass: 'custom-loader-class'
     });
     await loader.present();
 
-    try {
-      const data = {
-        fullName: this.fullName,
-        staffNumber: this.staffNumber,
-        email: this.email,
-        position: this.position,
-        department: this.department,
-      };
 
-      await this.afs.collection('lecturer').add(data);
 
-      // Clear the input fields
-      this.fullName = '';
-      this.staffNumber = '';
-      this.email = '';
-      this.position = '';
-      this.department = '';
+    this.auth.createUserWithEmailAndPassword(this.email, this.staffNumber)
+      .then(userCredential => {
+         this.firestore.collection('registered staff').add({
+          email: this.email,
+          fullName: this.fullName,
+          staffNumber : this.staffNumber,
+          position: this.position,
+          department: this.department,
 
-      loader.dismiss();
-      alert('Information successfully saved');
-    } catch (error) {
-      loader.dismiss();
-      console.error('Error saving data:', error);
-      alert('An error occurred while saving the information.');
-    }
+        });
+        loader.dismiss();
+
+      
+
+
+        this.router.navigateByUrl("/login");
+        this.presentToast()
+        
+        // ...
+      })
+      .catch((error) => {
+        loader.dismiss();
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        if(errorMessage=="Firebase: Error (auth/missing-email)."){
+
+        }else if(errorMessage=="Firebase: The email address is badly formatted. (auth/invalid-email)."){
+          alert("badly formatted e email");
+        }else if(errorMessage=="Firebase: The email address is already in use by another account. (auth/email-already-in-use)."){
+          alert("invalid email or password");
+        }
+        else if(errorMessage=="Firebase: There is no user record corresponding to this identifier. The user may have been deleted. (auth/user-not-found)."){
+          alert("invalid email");
+        }else{
+          alert(errorMessage);
+        }
+
+      });
+
+  }
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'successfully registered!',
+      duration: 1500,
+      position: 'top'
+    });
+
+  }
   }
 
-}
+
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { NavController, LoadingController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,16 +9,115 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  email!: string;
-  name!: string;
-  surname!: string;
-  studentNumber!: number;
-  password!: string;
-  constructor() { }
+  email: any;
+  password: any;
+  emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // regular expression for email validation
+  passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/; // regular expression for password validation
+  userData: any;
+  staffNumber: any;
+
+  constructor(private db: AngularFirestore,private loadingController: LoadingController,
+    private auth: AngularFireAuth,private navController: NavController,
+    private toastController: ToastController) { }
 
   ngOnInit() {
   }
-login(){
+
+  async validate() {
+    // Validate input
+    if (!this.email) {
+      const toast = await this.toastController.create({
+        message: 'Please enter your email.',
+        duration: 2000,
+        color: 'danger'
+      });
+      toast.present();
+      return;
+    }
+  
+    // Validate email format
+    if (!this.emailRegex.test(this.email)) {
+      const toast = await this.toastController.create({
+        message: 'Please provide a valid email address.',
+        duration: 2000,
+        color: 'danger'
+      });
+      toast.present();
+      return;
+    }
+  
+    // Validate password format
+    if (!this.password) {
+      const toast = await this.toastController.create({
+        message: 'Please enter your password.',
+        duration: 2000,
+        color: 'danger'
+      });
+      toast.present();
+      return;
+    }
+  
+    // If all validations pass, continue with sign-in logic
+    //this.login();
+  }
+
+  
+  async login() {
+    const loader = await this.loadingController.create({
+      message: 'Signing in',
+      cssClass: 'custom-loader-class'
+    });
+  
+    try {
+      await loader.present();
+  
+      // Attempt to sign in using email and password
+      const userCredential = await this.auth.signInWithEmailAndPassword(this.email, this.password);
+  
+      if (userCredential) {
+        // Check if the user is a registered student
+        const studentQuerySnapshot = await this.db.collection('registeredStudents')
+          .ref.where('email', '==', this.email)
+          .get();
+  
+        if (!studentQuerySnapshot.empty) {
+          // User is a registered student
+          loader.dismiss();
+          this.navController.navigateForward('/qr-scan');
+          return; // Exit the function
+        }
+  
+        // Check if the user is a registered staff member
+        const staffQuerySnapshot = await this.db.collection('registered staff')
+          .ref.where('email', '==', this.email).where('staffNumber', '==', this.password)
+          .get();
+  
+        if (!staffQuerySnapshot.empty) {
+          // User is a registered staff member
+          loader.dismiss();
+          this.navController.navigateForward('/lecture');
+          return; // Exit the function
+        }
+  
+        // User not found in either collection
+        loader.dismiss();
+        alert('User not found in registeredStudents or registeredStaff collections.');
+      }
+    } catch (error) {
+      loader.dismiss();
+      const errorMessage = (error as Error).message;
+  
+      if (errorMessage === 'Firebase: The password is invalid or the user does not have a password. (auth/wrong-password)' ||
+        errorMessage === 'Firebase: There is no user record corresponding to this identifier. The user may have been deleted. (auth/user-not-found)') {
+        alert('Invalid email or password');
+      } else if (errorMessage === 'Firebase: The email address is badly formatted. (auth/invalid-email)') {
+        alert('Incorrectly formatted email');
+      }
+    }
+  }
+  
+  
   
 }
-}
+
+
