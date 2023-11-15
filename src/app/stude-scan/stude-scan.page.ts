50f8c84960cece52a18d359755bf9b6f625dc2b0
MUTInnovationLab <mutinnovationlab@gmail.com>
2023-11-15 11:27:49 +0200

    mine

--- src/app/stude-scan/stude-scan.page.ts
+++ src/app/stude-scan/stude-scan.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-stude-scan',
@@ -7,19 +10,60 @@ import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
   styleUrls: ['./stude-scan.page.scss'],
 })
 export class StudeScanPage implements OnInit {
+  constructor(
+    private firestore: AngularFirestore,
+    private auth: AngularFireAuth
+  ) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   async scanBarcode() {
-    const result = await BarcodeScanner.startScan();
-    if (result.hasContent) {
-      console.log('Scanned data:', result.content);
-    } else {
-      console.error('No barcode data found.');
+    try {
+      const result = await BarcodeScanner.startScan();
+      if (result.hasContent) {
+        console.log('Scanned data:', result.content);
+
+        // Get the currently logged-in user
+        const user = await this.auth.currentUser;
+
+        if (user) {
+          // Use the user's email as the ID
+          const userId: string = user.email || ''; // Provide a default value if email is null
+
+          // Fetch additional details from the "registeredStudents" collection
+          const studentRef: AngularFirestoreDocument<any> = this.firestore.collection('registeredStudents').doc(userId)!;
+
+          // Ensure studentRef is not undefined before calling get()
+          if (studentRef) {
+            const studentDetails = await studentRef.get().pipe(first()).toPromise();
+
+            if (studentDetails && studentDetails.exists) {
+              // Combine scanned data and additional details
+              const attendanceDetails = {
+                email: user.email,
+                studentNumber: studentDetails.data().studentNumber,
+                name: studentDetails.data().name,
+                surname: studentDetails.data().surname,
+                scanDate: new Date(), // Include the date the QR code was scanned
+              };
+
+              // Store attendance in the Firestore "Attendance" collection
+              await this.firestore.collection('Attendance').doc(userId).set(attendanceDetails);
+              console.log('Attendance stored successfully.');
+            } else {
+              console.error('Student details not found in registeredStudents collection.');
+            }
+          } else {
+            console.error('Student reference is undefined.');
+          }
+        } else {
+          console.error('User not logged in.');
+        }
+      } else {
+        console.error('No barcode data found.');
+      }
+    } catch (error) {
+      console.error('Barcode scanning error:', error);
     }
   }
-
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAohbllLwFHIPOllSp8011M7_u_zqRvZx0",
+    authDomain: "student-attendance-7453a.firebaseapp.com",
+    projectId: "student-attendance-7453a",
+    storageBucket: "student-attendance-7453a.appspot.com",
+    messagingSenderId: "172755450483",
+    appId: "1:172755450483:web:2224bb16b714f3c514c806"
+  }
 };
